72021174ae9c3fccddf08454e758cc34626a8de7
Nadrieril <nadrieril@gmail.com>
2019-01-10 21:04:08 +0100

    Add a playout test

--- tests/game_test.py
+++ tests/game_test.py
@@ -6,6 +6,23 @@ from cubirds.game_analysis import available_lays, available_flocks
 from random_moves import playout
 
 
+def test_game_example():
+    game = Game(n_players=1, n_rows=1)
+    game.deck = Stack(['sparrow'])
+    game.board[0] = ['parrot', 'parrot', 'parrot', 'parrot', 'cube']
+    game.hands[0] = Stack(['cube', 'cube', 'sandwich'])
+    assert game.current_phase == 'lay'
+
+    game.lay('cube', 0, 'left')
+    assert game.current_phase == 'flock'
+    assert game.deck.l == []
+    assert game.board[0] == ['cube', 'cube', 'cube', 'sparrow']
+
+    game.flock('parrot')
+    assert game.current_phase == 'lay'
+    assert game.hands[0].l == ['sandwich']
+    assert 'parrot' in game.collections[0].l
+
 def test_perf_available_lays(benchmark):
     game = Game(1, 4)
     hand = game.current_hand
